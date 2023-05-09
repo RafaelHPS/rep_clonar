92c95f75601417d48823feb3fd2dd96c1c14c724
RafaelHPS <rafael.souza123@fatec.sp.gov.br>
2023-05-09 18:00:12 +0000

    Replace nomes do projeto para pode ser usado no cookiecutter

--- {{cookiecutter.nome_projeto}}/{{cookiecutter.nome_projeto}}/classes_t2c/T2CKillAllProcesses.py
+++ {{cookiecutter.nome_projeto}}/{{cookiecutter.nome_projeto}}/classes_t2c/T2CKillAllProcesses.py
@@ -1,10 +1,11 @@
 from botcity.web import WebBot, Browser
 from botcity.core import DesktopBot
-from framework.classes_t2c.utils.T2CMaestro import T2CMaestro, LogLevel
-from framework.classes_t2c.utils.T2CExceptions import BusinessRuleException
+from {{cookiecutter.nome_projeto}}.classes_t2c.utils.T2CMaestro import T2CMaestro, LogLevel
+from {{cookiecutter.nome_projeto}}.classes_t2c.utils.T2CExceptions import BusinessRuleException
 
-#Classe feita para ser invocada no final da automação, para fechar todos os aplicativos
-class T2CCloseAllApplications:
+#Classe feita para ser invocada em casos de system exceptions no processamento, para resetar o processamento
+#Pode ser usado em outras partes do processo também, dependendo de como a automação for programada
+class T2CKillAllProcesses:
     #Iniciando a classe, pedindo um dicionário config e o bot que vai ser usado e enviando uma exceção caso nenhum for informado
     def __init__(self, arg_dictConfig:dict, arg_clssMaestro:T2CMaestro, arg_botWebbot:WebBot=None, arg_botDesktopbot:DesktopBot=None):
         if(arg_botWebbot is None and arg_botDesktopbot is None): raise Exception("Não foi possível inicializar a classe, forneça pelo menos um bot")
@@ -14,18 +15,18 @@ class T2CCloseAllApplications:
             self.var_dictConfig = arg_dictConfig
             self.var_clssMaestro = arg_clssMaestro
 
-    #Método que roda para fechar os programas necessários, apenas com a estrutura em código
+    #Método que roda para finalizar os processos necessários, apenas com a estrutura em código
     def execute(self):
         #Edite o valor dessa variável a no arquivo Config.xlsx
         var_intMaxTentativas = self.var_dictConfig["MaxRetryNumber"]
 
         for var_intTentativa in range(var_intMaxTentativas):
             try:
-                self.var_clssMaestro.write_log("Finalizando todos os processos, tentativa " + (var_intTentativa+1).__str__())
-                #Insira aqui seu código para fechar os aplicativos
+                self.var_clssMaestro.write_log("Finalizando processos, tentativa " + (var_intTentativa+1).__str__())
+                #Insira aqui seu código para finalizar processos
             except BusinessRuleException as exception:
                 self.var_clssMaestro.write_log(arg_strMensagemLog="Erro de negócio: " + str(exception), arg_enumLogLevel=LogLevel.ERROR)
- 
+
                 raise
             except Exception as exception:
                 self.var_clssMaestro.write_log(arg_strMensagemLog="Erro, tentativa " + (var_intTentativa+1).__str__() + ": " + str(exception), arg_enumLogLevel=LogLevel.ERROR)
